64f1e57266e426dd9c865d00794a45794ab57f41
mamoze <47969414+mamoze@users.noreply.github.com>
2019-03-25 12:27:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function greet(){
-    window.alert("Hello there!");
+    window.alert("Ici, ici c’est La Rochelle, ici ici c’est La Rochelle.\n" +
+        "Avec le jaune et noir que tu es belle.\n" +
+        "Et ton ovalie te rend plus rebelle.\n" +
+        "\n" +
+        "A DEFLANDRE sur sa pelouse que beaucoup nous jalousent.\n" +
+        "Pas le temps d’aller aux cagouilles, ni de s’amuser à la pêche aux moules.\n" +
+        "Après une sauce aux lumas, aucun essai ne se marquera.\n" +
+        "Mais avec un cantaloup au pineau, tout repart pour aller à l’assaut.\n" +
+        "Poussez…poussez les avants Rochelais.\n" +
+        "\n" +
+        "Ici.. ici c’est La Rochelle, ici…ici c’est La Rochelle.\n" +
+        "Allez les jaunes, allez les noirs…allez les jaunes et noirs.\n" +
+        "Avec notre beurre les avants et arrières font taire les plus coriaces adversaires.\n" +
+        "\n" +
+        "Au palais les huitres font merveilles\n" +
+        "Mais rare est d’enfiler leurs perles.\n" +
+        "Alors qu’entre nos tours, plus aisé, serait de voir s’enquiller drops et essais\n" +
+        "Pour enfin arracher la victoire et nous donner une légitime gloire.\n" +
+        "\n" +
+        "Ici.. ici c’est La Rochelle, ici…ici c’est La Rochelle.\n" +
+        "Vive La Rochelle et le stade Rochelais.");
+}
+function init(){
+    document.getElementById("first").src = "flower2.png";
 }
